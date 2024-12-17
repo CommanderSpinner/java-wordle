5fbcf86d1d5efe4937d3632d8ad651fd9116f8c3
CommanderSpinner <123424514+CommanderSpinner@users.noreply.github.com>
2024-12-17 01:38:43 +0100

    Update Wordl.java

--- src/main/java/com/mycompany/wordl/Wordl.java
+++ src/main/java/com/mycompany/wordl/Wordl.java
@@ -2,10 +2,6 @@ package com.mycompany.wordl;
 
 import java.sql.SQLException;
 
-/**
- *
- * @author Lorenz Schaller
- */
 public class Wordl {
 
     public static void main(String[] args) throws SQLException {
