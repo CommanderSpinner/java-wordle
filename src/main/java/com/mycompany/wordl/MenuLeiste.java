9570c141aacb847955bbeb295faf4b1732dc8486
CommanderSpinner <123424514+CommanderSpinner@users.noreply.github.com>
2024-12-17 01:37:17 +0100

    Update MenuLeiste.java

--- src/main/java/com/mycompany/wordl/MenuLeiste.java
+++ src/main/java/com/mycompany/wordl/MenuLeiste.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.wordl;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-/**
- *
- * @author Lorenz Schaller
- */
 public class MenuLeiste extends JMenuBar{
     JMenu optionen;
     JMenuItem item_register;
