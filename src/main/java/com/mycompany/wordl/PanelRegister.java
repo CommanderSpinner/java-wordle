bd148286ac68048256f62d519812449bc76dbcd6
CommanderSpinner <123424514+CommanderSpinner@users.noreply.github.com>
2024-12-17 01:38:01 +0100

    Update PanelRegister.java

--- src/main/java/com/mycompany/wordl/PanelRegister.java
+++ src/main/java/com/mycompany/wordl/PanelRegister.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.wordl;
 
 import javax.swing.JButton;
@@ -9,10 +5,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-/**
- *
- * @author Lorenz Schaller
- */
 public class PanelRegister extends JPanel{
     JButton btn_Register;
     JTextField txt_NickName;
